2ec0c9ca5e476d3f023a5a2d2e0d50e3fa56f9eb
brizzaa <brizzantex@gmail.com>
2025-02-25 19:31:18 +0100

    feat : correzione

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,7 +10,8 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.reverse();
+const reversedTeachers = Array.from(teachers);
+reversedTeachers.reverse()
 console.log(reversedTeachers);
 
 
